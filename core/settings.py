a1db59f9eb82c46b176743e76dbd060b84036606
betensis <betensis@gmail.com>
2021-10-12 21:39:14 +0200

    добавил взяфтие секретного ключа из переменных окружения

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qn0n$_00#*h_7gl5^at6^-mhqx(iu07jv4ji+p07nmtsk)o^it"
+SECRET_KEY = env.get_str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.get_bool("DEBUG", False)
