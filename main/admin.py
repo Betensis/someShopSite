4af4f776acf15b3bdaabce4a231af84d69e07a5f
betensis <betensis@gmail.com>
2021-10-12 21:39:57 +0200

    Создал модель MainCategory и SubCategory

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MainCategory, Subcategory
+
+admin.site.register(MainCategory)
+admin.site.register(Subcategory)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class MainCategory(models.Model):
+    name = models.CharField(
+        verbose_name=_("name"),
+        max_length=150,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(
+        verbose_name=_("name"),
+        max_length=150,
+        unique=True,
+    )
+    main_category = models.ForeignKey(
+        MainCategory,
+        on_delete=models.CASCADE,
+        related_name=_("subcategory"),
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
