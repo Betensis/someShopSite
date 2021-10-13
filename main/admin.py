45415349a6d99a61b3e990f3c639e08e2f8496db
betensis <betensis@gmail.com>
2021-10-13 15:51:42 +0200

    зарегистрировал в админке модели MainCategory, Subcategory, Shoes

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from .models import MainCategory, Subcategory
+from .models import MainCategory, Subcategory, Shoes
 
-admin.site.register(MainCategory)
-admin.site.register(Subcategory)
+
+@admin.register(MainCategory)
+class MainCategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "main_category"]
+
+
+@admin.register(Shoes)
+class ShoesAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "price", "category", "main_category"]
+
+    @admin.display(description="Основная категория")
+    def main_category(self, obj):
+        return obj.category.main_category
