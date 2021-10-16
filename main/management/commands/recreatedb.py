bf1aa841bd84fc60e876f7668c114aba7befd19d
betensis <betensis@gmail.com>
2021-10-17 01:13:23 +0200

    Сделал удаление базы данных в recreatedb вместо очистки бд

--- main/management/commands/recreatedb.py
+++ main/management/commands/recreatedb.py
@@ -1,12 +1,19 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
+from core import settings
+
+
+def delete_db():
+    if settings.DEBUG:
+        settings.DATABASES["default"]["NAME"].unlink(missing_ok=True)
+
 
 class Command(BaseCommand):
     help = "Recreates migrations and creates a database."
 
     def handle(self, *args, **options):
-        call_command("flush", database="default")
+        delete_db()
         call_command("deletemigrationsdir")
         call_command("makemigrations", "main")
         call_command("migrate", "--run-syncdb")
