d691255252488ba210ecbe515decd66b35bc3a11
betensis <betensis@gmail.com>
2021-10-16 14:58:04 +0200

    Создал общий класс в админке для Products

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import MainCategory, Subcategory, Shoes
+from .models import MainCategory, Subcategory, Shoes, Brand, Hat
 
 
 @admin.register(MainCategory)
@@ -13,10 +13,15 @@ class SubcategoryAdmin(admin.ModelAdmin):
     list_display = ["title", "main_category"]
 
 
-@admin.register(Shoes)
-class ShoesAdmin(admin.ModelAdmin):
+@admin.register(Shoes, Hat)
+class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "description", "price", "category", "main_category"]
 
     @admin.display(description="Основная категория")
     def main_category(self, obj):
         return obj.category.main_category
+
+
+@admin.register(Brand)
+class Brand(admin.ModelAdmin):
+    list_display = ["title"]
