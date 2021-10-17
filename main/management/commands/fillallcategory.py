1c783f368bf32bc9d3bae3ade2b5b98ccefb85e7
betensis <betensis@gmail.com>
2021-10-17 23:55:17 +0200

    Удалил команду fillmaincategory и заменил ее на fillallcategory

---
+++ main/management/commands/fillallcategory.py
@@ -0,0 +1,32 @@
+from autoslug.utils import slugify
+from django.core.management import BaseCommand
+
+from main.models import MainCategory, Subcategory
+
+default_categories = {
+    "Головные уборы": [
+        "Кепки",
+        "Банданы",
+    ],
+    "Обувь": [
+        "Кроссовки",
+        "Сандали",
+    ],
+    "Верхняя одежда": [
+        "Куртки",
+        "Футболки",
+    ],
+    "Аксессуары": ["Брелки", "Чехлы"],
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for main_category, subcategories in default_categories.items():
+            main_category, _ = MainCategory.objects.get_or_create(title=main_category)
+            for subcategory in subcategories:
+                Subcategory.objects.get_or_create(
+                    title=subcategory,
+                    main_category=main_category,
+                    slug=slugify(subcategory),
+                )
