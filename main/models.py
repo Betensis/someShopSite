a6d7ae2a1eb26eedd33cff76127b21f5e6e395f9
betensis <betensis@gmail.com>
2021-10-18 20:34:35 +0200

    переименовал все verbose_name verbose_name_plural. добивал модель Order OrderItem

--- main/models.py
+++ main/models.py
@@ -1,5 +1,8 @@
 from autoslug import AutoSlugField
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from djmoney.models.fields import MoneyField
@@ -10,8 +13,8 @@ User = get_user_model()
 
 class MainCategory(models.Model):
     class Meta:
-        verbose_name = _("Основная категория")
-        verbose_name_plural = _("Основные категории")
+        verbose_name = _("основная категория")
+        verbose_name_plural = _("основные категории")
 
     title = models.CharField(
         verbose_name=_("название"),
@@ -31,8 +34,8 @@ class MainCategory(models.Model):
 
 class Subcategory(models.Model):
     class Meta:
-        verbose_name = _("Подкатегория")
-        verbose_name_plural = _("Подкатегории")
+        verbose_name = _("подкатегория")
+        verbose_name_plural = _("подкатегории")
 
     title = models.CharField(
         verbose_name=_("название"),
@@ -121,11 +124,33 @@ class Item(models.Model):
 
 class Shoes(Item):
     class Meta:
-        verbose_name = _("Обувь")
-        verbose_name_plural = _("Обувь")
+        verbose_name = _("обувь")
+        verbose_name_plural = _("обувь")
 
 
 class Hat(Item):
     class Meta:
         verbose_name = _("головной убор")
         verbose_name_plural = _("головные уборы")
+
+
+class OrderItem(models.Model):
+    customer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    ordered = models.BooleanField(default=False)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    quantity = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        User,
+        models.CASCADE,
+    )
+    items = models.ManyToManyField(OrderItem)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField()
