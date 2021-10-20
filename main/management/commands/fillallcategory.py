1d1a948af57f5dabcf9e0815b05b92b1f1bc866a
betensis <betensis@gmail.com>
2021-10-20 11:01:02 +0200

    переделал fillallcategory

--- main/management/commands/fillallcategory.py
+++ main/management/commands/fillallcategory.py
@@ -1,22 +1,22 @@
 from autoslug.utils import slugify
 from django.core.management import BaseCommand
+from django.db import IntegrityError
 
 from main.models import MainCategory, Subcategory
 
-default_categories = {
-    "Головные уборы": [
-        "Кепки",
-        "Банданы",
+default_categories: dict[MainCategory : list[Subcategory]] = {
+    MainCategory(title="Головные уборы", slug="hat", pk=1): [
+        Subcategory(title="Кепки", slug="cap"),
+        Subcategory(title="Банданы", slug="bandana"),
     ],
-    "Обувь": [
-        "Кроссовки",
-        "Сандали",
+    MainCategory(title="Обувь", slug="shoes", pk=2): [
+        Subcategory(title="Кроссовки", slug="sneakers"),
+        Subcategory(title="Сандали", slug="sandals"),
     ],
-    "Верхняя одежда": [
-        "Куртки",
-        "Футболки",
+    MainCategory(title="Верхняя одежда", slug="outerwear", pk=3): [
+        Subcategory(title="Куртки", slug="jacket"),
+        Subcategory(title="Футболки", slug="t-shirt"),
     ],
-    "Аксессуары": ["Брелки", "Чехлы"],
 }
 
 
@@ -25,10 +25,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for main_category, subcategories in default_categories.items():
-            main_category, _ = MainCategory.objects.get_or_create(title=main_category)
+            try:
+                main_category.save()
+            except IntegrityError:
+                pass
             for subcategory in subcategories:
-                Subcategory.objects.get_or_create(
-                    title=subcategory,
-                    main_category=main_category,
-                    slug=slugify(subcategory),
-                )
+                subcategory.main_category = main_category
+                try:
+                    subcategory.save()
+                except IntegrityError:
+                    pass
