215dec4c49e88bc3bd8e360403aa6a3db1091f75
betensis <betensis@gmail.com>
2021-10-21 22:57:20 +0200

    refactor fillallcategory

--- main/management/commands/fillallcategory.py
+++ main/management/commands/fillallcategory.py
@@ -1,36 +1,54 @@
+from dataclasses import dataclass
+
 from autoslug.utils import slugify
 from django.core.management import BaseCommand
 from django.db import IntegrityError
 
 from main.models import MainCategory, Subcategory
 
-default_categories: dict[MainCategory: list[Subcategory]] = {
-    MainCategory(title="Головные уборы", slug="hat", pk=1): [
-        Subcategory(title="Кепки", slug="cap"),
-        Subcategory(title="Банданы", slug="bandana"),
-    ],
-    MainCategory(title="Обувь", slug="shoes", pk=2): [
-        Subcategory(title="Кроссовки", slug="sneakers"),
-        Subcategory(title="Сандали", slug="sandals"),
-    ],
-    MainCategory(title="Верхняя одежда", slug="outerwear", pk=3): [
-        Subcategory(title="Куртки", slug="jacket"),
-        Subcategory(title="Футболки", slug="t-shirt"),
-    ],
-}
+
+@dataclass(frozen=True)
+class Categories:
+    main_category: MainCategory
+    subcategories: list[Subcategory]
+
+
+default_categories: list[Categories] = [
+    Categories(
+        MainCategory(title="Головные уборы", slug="hat"),
+        [
+            Subcategory(title="Кепки", slug="cap"),
+            Subcategory(title="Банданы", slug="bandana"),
+        ],
+    ),
+    Categories(
+        MainCategory(title="Обувь", slug="shoes"),
+        [
+            Subcategory(title="Кроссовки", slug="sneakers"),
+            Subcategory(title="Сандали", slug="sandals"),
+        ],
+    ),
+    Categories(
+        MainCategory(title="Верхняя одежда", slug="outerwear"),
+        [
+            Subcategory(title="Куртки", slug="jacket"),
+            Subcategory(title="Футболки", slug="t-shirt"),
+        ],
+    ),
+]
 
 
 class Command(BaseCommand):
     help = f"Fills the database with the following categories: {default_categories}"
 
     def handle(self, *args, **options):
-        for main_category, subcategories in default_categories.items():
+        for category in default_categories:
             try:
-                main_category.save()
+                category.main_category.save()
             except IntegrityError:
                 pass
-            for subcategory in subcategories:
-                subcategory.main_category = main_category
+            for subcategory in category.subcategories:
+                subcategory.main_category = category.main_category
                 try:
                     subcategory.save()
                 except IntegrityError:
