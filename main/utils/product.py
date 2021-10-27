a6573e43082328f03706ca3f36daeddf6e08918d
betensis <betensis@gmail.com>
2021-10-27 17:12:41 +0200

    добавил комментарии в get_product_sub_models. Там слишком много магии

--- main/utils/product.py
+++ main/utils/product.py
@@ -5,10 +5,15 @@ import inspect
 
 
 def get_product_sub_models() -> dict[str, Type[models.Product]]:
+    """
+    Возвращает словарь, где сопостовляется имя наследников Product и их класс
+    """
+    # Выбираем все подклассы Product
     product_models = filter(
         lambda x: inspect.isclass(x[1]) and issubclass(x[1], models.Product),
         models.__dict__.items(),
     )
+    # Удаляем из product_models класс Product
     product_models_subclasses = filter(
         lambda x: x[1] is not models.Product, product_models
     )
