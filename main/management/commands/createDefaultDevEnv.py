4059a88cf45778dbfc1eb36eafb2b760106649e2
betensis <betensis@gmail.com>
2022-01-02 23:47:08 +0200

    syntax

--- main/management/commands/createDefaultDevEnv.py
+++ main/management/commands/createDefaultDevEnv.py
@@ -5,5 +5,5 @@ class Command(BaseCommand):
     help = "Create default development environment: recreate db and fill it. Also create default admin"
 
     def handle(self, *args, **options):
-        call_command('recreateAndFillDB')
-        call_command('createDefaultAdmin')
+        call_command("recreateAndFillDB")
+        call_command("createDefaultAdmin")
--- main/management/commands/recreateAndFillDB.py
+++ main/management/commands/recreateAndFillDB.py
@@ -2,8 +2,8 @@ from django.core.management import BaseCommand, call_command
 
 
 class Command(BaseCommand):
-    help = "recreate db and fill categories and brands"
+    help = "Recreate db and fill categories and brands"
 
     def handle(self, *args, **options):
         call_command("recreateDB")
-        call_command('fillProducts')
+        call_command("fillProducts")
